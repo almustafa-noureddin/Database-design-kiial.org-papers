8d1c470a9f2894f0abf9296af8d5a9ebda2d338b
almustafa-noureddin <58104742+almustafa-noureddin@users.noreply.github.com>
2022-03-14 23:38:00 +0200

    Buiding custom filter using django-filter module
    built custom filter and created seperate view that comes with module
    styled the filter forms using bootstrap and django-widget-tweaks module

--- django_application/kiial_papers/settings.py
+++ django_application/kiial_papers/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'papers',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
--- django_application/kiial_papers/urls.py
+++ django_application/kiial_papers/urls.py
@@ -1,6 +1,7 @@
 """
 kiial_papers URL Configuration
 """
+from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -14,15 +15,18 @@ from django.contrib.auth.views import (
 )
 from django.contrib import admin
 from django.urls import path,include
-#from papers.views import (
-#    landing_page, 
+from django_filters.views import FilterView
+from papers.filters import PapersFilter
+from papers.views import (
+    IndexView, 
 #    LandingPageView, 
 #    SignupView, 
 #    DashboardView
-#)
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', LandingPageView.as_view(), name='landing-page'),
-    
+    path('', IndexView.as_view(), name='index'),
+    url(r'^search/$', FilterView.as_view(filterset_class=PapersFilter,
+        template_name='papers/search.html'), name='search'),
 ]
